cbc41f7c21a4a6a316a0d00fdf0f7790d61963d1
MohamedMohamed <mohamed.m.abdalfattah@gmail.com>
2024-09-26 06:20:58 +0300

    Some Styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,14 +5,14 @@ const Home = () => (
         <div className="container">
             <div className="home-content">
                 <div className="text-1" data-aos="zoom-in">Hello, Let me to introduce</div>
-                <div className="text-2" style={{ color: '#DAC0A3', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.9)' }} data-aos="zoom-in">
+                <div className="text-2" data-aos="zoom-in">
                     Mohamed Mohamed
                 </div>
                 <div className="text-3" data-aos="zoom-in">
                     {`And I'm a `}
                     <span className="typing" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.9)' }}></span>
                 </div>
-                <a href="#contact" data-aos="flip-down">Contact me</a>
+                <a href="#contact" data-aos="flip-down"><p className='bt_contact'>Contact me</p></a>
             </div>
         </div>
     </section>
